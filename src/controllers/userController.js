c3b79b25a3ccd1c02da289445ddb60bccf00f9a3
Shreya131998 <singh.shreya0741416@gmail.com>
2022-05-25 12:32:48 +0530

    user done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,7 +53,9 @@ const isValidRequestBody = function (value) {
 const createUser =async (req,res)=>{
 
     try {
+
         let data = req.body
+        data=JSON.parse(JSON.stringify(data))  
         if (!isValidRequestBody(data)) {
             res.status(400).send({ status: false, message: "invalid request parameters.plzz provide user details" })
             return
@@ -116,6 +118,7 @@ const createUser =async (req,res)=>{
         if (!isValid(password)) {
             return res.status(400).send({ status: false, message: "plzz enter password" })
         }
+        password=password.trim()
         if (password.length < 8 || password.length > 15) {
             return res.status(400).send({ status: false, message: "plzz enter valid password" })
         }
@@ -133,7 +136,7 @@ const createUser =async (req,res)=>{
         }
 
         //this regex will to set the phone no. length to 10 numeric digits only.
-        if (!/^(\+91)?0?[6-9]\d{9}$/.test(phone)) {
+        if (!/^(\+91)?0?[6-9]\d{9}$/.test(phone.trim())) {
             return res.status(400).send({ status: false, message: "Please enter valid 10 digit mobile number." })
         }
 
@@ -148,15 +151,16 @@ const createUser =async (req,res)=>{
         if(!address){
             return res.status(400).send({ status: false, message: "address is required" })
         }
+        data.address=JSON.parse(data.address)
         
-         address=JSON.parse(address)
-         console.log(address)
-         console.log(typeof address)
+         //address=JSON.parse(address)
+         //console.log(address)
+         //console.log(typeof address)
 
-        if (typeof address != "object") {
+        if (typeof data.address != "object") {
                 return res.status(400).send({ status: false, message: "address should be an object" })
             }
-        let { shipping,billing} = address
+        let { shipping,billing} = data.address
         
         console.log(typeof shipping)
         console.log(shipping)
@@ -223,8 +227,8 @@ const createUser =async (req,res)=>{
                     return res.status(400).send({ status: false, message: "plz enter valid  billing pincode" });
                 }
                
-        data.address=JSON.parse(data.address)   //DOUBT
-        data=JSON.parse(JSON.stringify(data))     
+           //DOUBT
+           
         console.log(data)       
 
         let saveData = await userModel.create(data)
@@ -353,15 +357,16 @@ const loginUser = async function (req, res) {
           if(!ObjectId.isValid(pathParams)){
               return res.status(400).send({status:false,message:"user id is not valid"})
           }
-          let user=await userModel.findOne({_id:pathParams}) 
+          let authorToken=req.userId
+          if(!ObjectId.isValid(authorToken)){
+              return res.status(400).send({status:false,message:"Please enter valid user token"})
+          }
+        let user=await userModel.findOne({_id:pathParams}) 
         if(!user){
             return res.status(404).send({status:false,message:"No user found"})
         }
 
-          let authorToken=req.userId
-        if(!ObjectId.isValid(authorToken)){
-            return res.status(400).send({status:false,message:"Please enter valid user token"})
-        }
+         
         if(authorToken!==pathParams){
             return res.status(403).send({status:false,message:"Unauthorized user"})
         }
@@ -374,19 +379,51 @@ const loginUser = async function (req, res) {
           let {email,phone,lname,fname,password,address}=data 
           console.log(address)
           if(email){
-            let emailCheck=  await userModel.findOne({email:email})
-            if(emailCheck){
-                return res.status(400).send({status:false,message:"Email is already registered"})
+            if (!isValid(email)) {
+                return res.status(400).send({ status: false, message: "plzz enter email" })
             }
+            email = email.toLowerCase().trim()
+            const emailPattern = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})/       //email regex validation for validate the type of email.
+    
+            if (!email.match(emailPattern)) {
+                return res.status(400).send({ status: false, message: "This is not a valid email" })
+            }
+    
+            
+            const emailExt = await userModel.findOne({ email: email })
+            if (emailExt) {
+                return res.status(400).send({ status: false, message: "Email already exists" })
+            }
+          
+            data.email=email
+        
+    }
 
-          }
           if(phone){
-            let phoneCheck=  await userModel.findOne({phone:phone})
-            if(phoneCheck){
-                return res.status(400).send({status:false,message:"Phone no is already registered"})
+            if (!isValid(phone)) {
+                return res.status(400).send({ status: false, message: "plzz enter mobile" })
             }
+            
+    
+            //this regex will to set the phone no. length to 10 numeric digits only.
+            if (!/^(\+91)?0?[6-9]\d{9}$/.test(phone.trim())) {
+                return res.status(400).send({ status: false, message: "Please enter valid 10 digit mobile number." })
+            }
+    
+            const phoneExt = await userModel.findOne({ phone: phone })
+            if (phoneExt) {
+                return res.status(400).send({ status: false, message: "phone number already exists" })
+            }
+          
+            
 
-          }
+        
+            
+            data.phone=phone
+        
+    }
+
+          
         console.log(address) 
         let file= req.files
         console.log(file)
@@ -397,72 +434,174 @@ const loginUser = async function (req, res) {
             data["profileImage"]=uploadedFileURL
         }
         
-        // if(isValid(fname)){
-        //     data.fname=fname 
-        // }
-        // if(isValid(lname)){
-        //     data.lname=lname
-        // }
-        // if(isValid(email)){
-        //     data.email=email
-        // }
-        // if(isValid(password)){
-        //     data.password=password
-        // }
+        if(fname){
+            if(!isValid(fname)){
+                return res.status(400).send({status:false,message:"Please enter valid fname"})
+            }
+            data.fname=fname 
+        }
+        if(lname){
+        if(!isValid(lname)){
+            return res.status(400).send({status:false,message:"Please enter valid lname"})
+            
+        }
+        data.lname=lname
+       }
+        if(password){
+            if (!isValid(password)) {
+                return res.status(400).send({ status: false, message: "plzz enter password" })
+            }
+            password=password.trim()
+            if (password.length < 8 || password.length > 15) {
+                return res.status(400).send({ status: false, message: "plzz enter valid password" })
+            }
+    
+    
+            const salt=await bcrypt.genSalt(10)
+            data.password=await bcrypt.hash(password,salt)
+            //console.log(data.password)
+    
+    
+        
+    
+    
+            
+        }
+       
+        
         if(address){
             console.log(1)
             data.address=JSON.parse(data.address)
-            if(typeof address =='object'){
-                let {shipping,billing}=address 
-                console.log(shipping)
-                console.log(typeof shipping)
+            console.log(typeof data.address)
+            if(typeof data.address !="object"){
+                return res.status(400).send({status:false,message:"Address must be in object"})
+
+            }
+            
+                let {shipping,billing}=data.address 
+                
+                // console.log(3)
+                // console.log(shipping)
+                // console.log(typeof shipping)
                 if(shipping){
-                    if(shipping.street){
+
+                    
+                    if(typeof shipping !="object"){
+                        return res.status(400).send({status:false,message:"Shipping must be in object"})
+                    } 
+                    if(Object.keys(shipping).length==0){
+                        return res.status(400).send({status:false,message:"No keys are given in shipping"})
+
+                    }
+                    let {street,city,pincode}=shipping
+                    if(street){
+                    if(!isValid(street)){
+                        return res.status(400).send({ status: false, message: "shipping street is required" })
+                    }
+                        
                         data.address.shipping.street=shipping.street
+                    
+                   }
+                   if(city){
+                    if (!isValid(shipping.city)) {
+                        return res.status(400).send({ status: false, message: "shipping city is required" })
                     }
-                    if(shipping.city){
-                        data.address.shipping.city=shipping.city
-                    }
-                    if(shipping.pincode){
-                        data.address.shipping.pincode=shipping.pincode
-                    }
-
-
-                }
-                if(billing){
-                    if(billing.street){
-                        data.address.billing.street=billing.street
-                    }
-                    if(billing.city){
-                        data.address.billing.city=billing.city
-                    }
-                    if(billing.pincode){
-                        data.address.billing.pincode=billing.pincode
+                    if (!/^[a-zA-Z]+$/.test(shipping.city)) {
+                        return res.status(400).send({ status: false, message: "city field have to fill by alpha characters" });
                     }
                     
+                        data.address.shipping.city=shipping.city
+                    
+                }   if(pincode){
+                    if (!isValid(pincode)) {
+                        return res.status(400).send({ status: false, message: "Shipping pincode is required" })
+                    }
+    
+                     //applicable only for numeric values and extend to be 6 characters only--
+                    if (!/^\d{6}$/.test(pincode)) {
+                        return res.status(400).send({ status: false, message: "plz enter valid  shipping pincode" });
+                    }
+                    
+                        data.address.shipping.pincode=shipping.pincode
+                    
+
+                
+
+
                 }
             }
-        }
+                
+            if(billing){
 
-        //let newData=Object.assign(user,data)
-        //console.log(newData)
-        
-        let updatedData=await userModel.findByIdAndUpdate({_id:pathParams},{fname:data.fname,lname:data.lname,
-            email:data.email,profileImage:data.profileImage,phone:data.phone,password:data.password,
-            address:{
-                shipping:{
-                    street:data.address?.shipping?.street || user.address.shipping.street ,
-                    city:data.address?.shipping?.city || user.address.shipping.city,
-                    pincode:data.address?.shipping?.pincode || user.address.shipping.pincode 
-                },
-                billing:{
-                    street:data.address?.billing?.street || user.address.billing.street ,
-                    city:data.address?.billing?.city || user.address.billing.city,
-                    pincode:data.address?.billing?.pincode || user.address.billing.pincode
+                    
+                if(typeof billing !="object"){
+                    return res.status(400).send({status:false,message:"billing must be in object"})
+                }
+                if(Object.keys(billing).length==0){
+                    return res.status(400).send({status:false,message:"No keys are given in billing"})
 
+                } 
+                let {street,city,pincode}=billing
+                if(street){
+                if(!isValid(street)){
+                    return res.status(400).send({ status: false, message: "billing street is required" })
+                }
+                    
+                    data.address.billing.street=billing.street
+                
+               }
+               if(city){
+                if (!isValid(billing.city)) {
+                    return res.status(400).send({ status: false, message: "billing city is required" })
+                }
+                if (!/^[a-zA-Z]+$/.test(billing.city)) {
+                    return res.status(400).send({ status: false, message: "city field have to fill by alpha characters" });
+                }
+                
+                    data.address.billing.city=billing.city
+                
+            }   if(pincode){
+                if (!isValid(pincode)) {
+                    return res.status(400).send({ status: false, message: "billing pincode is required" })
                 }
 
-            }},{new:true})
+                 //applicable only for numeric values and extend to be 6 characters only--
+                if (!/^\d{6}$/.test(billing.pincode)) {
+                    return res.status(400).send({ status: false, message: "plz enter valid  billing pincode" });
+                }
+                
+                    data.address.billing.pincode=billing.pincode
+                
+
+            
+
+
+            }
+        }
+            
+            
+
+            
+        }
+        data.address={
+            shipping:{
+                street:data.address?.shipping?.street || user.address.shipping.street ,
+                city:data.address?.shipping?.city || user.address.shipping.city,
+                pincode:data.address?.shipping?.pincode || user.address.shipping.pincode 
+            },
+            billing:{
+                street:data.address?.billing?.street || user.address.billing.street ,
+                city:data.address?.billing?.city || user.address.billing.city,
+                pincode:data.address?.billing?.pincode || user.address.billing.pincode
+
+            }
+
+        }
+
+        
+        
+        
+        let updatedData=await userModel.findByIdAndUpdate({_id:pathParams},data,{new:true})
         return res.status(200).send({status:true,data:updatedData})
         
 
